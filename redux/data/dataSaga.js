b0d4f1b21d71c4e125d7af821dc32465b2350558
WiktorMonka <wiktormonka@icloud.com>
2020-02-02 19:47:17 +0100

    add alfa calculate avg compsumption

--- redux/data/dataSaga.js
+++ redux/data/dataSaga.js
@@ -5,8 +5,107 @@ import axios from 'axios';
 import {GET_DATA_REQUEST} from './dataActions';
 import {deleteRequest} from './dataActions';
 
-function testt() {
-  console.log('testt');
+function calculateAvgConsumption(data) {
+  console.log(data);
+  let refuellingList = [];
+  Object.keys(data.refuellings).map(refuelling => {
+    refuellingList.push(data.refuellings[refuelling]);
+  });
+  console.log(refuellingList);
+
+  //CALC AVG FUEL CONSUMPTION
+  let refuellingfArray = [];
+  let refuellingfArray2 = [];
+  console.log(refuellingList);
+  refuellingList.reverse().forEach((refuelling, index) => {
+    let kmsLastRef = null;
+    let kmsLastFullRef = null;
+    let refueledLiters = 0;
+    let avgFuelConsumption = '-';
+    let loopCounter = 1;
+    let loopActive = true;
+
+    if (refuellingList.length === index + 1) {
+      avgFuelConsumption = '-';
+    }
+
+    if (refuellingList[index + 1] !== undefined) {
+      kmsLastRef =
+        Number(refuelling.mileage) - Number(refuellingList[index + 1].mileage);
+
+      if (refuelling.fullRefuelling === true) {
+        if (refuellingList[index + 1].fullRefuelling === true) {
+          kmsLastFullRef =
+            Number(refuelling.mileage) -
+            Number(refuellingList[index + 1].mileage);
+          refueledLiters = Number(refuelling.liters);
+          avgFuelConsumption = (
+            (refueledLiters / kmsLastFullRef) *
+            100
+          ).toFixed(2);
+        } else {
+          kmsLastFullRef +=
+            refuellingList[index].mileage - refuellingList[index + 1].mileage;
+          refueledLiters += Number(refuellingList[index].liters);
+          while (loopActive) {
+            if (refuellingList[index + loopCounter].fullRefuelling === false) {
+              kmsLastFullRef +=
+                Number(refuellingList[index + loopCounter].mileage) -
+                Number(refuellingList[index + (loopCounter + 1)].mileage);
+              refueledLiters += Number(
+                refuellingList[index + loopCounter].liters,
+              );
+              loopCounter++;
+            } else {
+              loopActive = false;
+              avgFuelConsumption = (
+                (refueledLiters / kmsLastFullRef) *
+                100
+              ).toFixed(2);
+            }
+          }
+        }
+      }
+    }
+
+    refuellingfArray.push(avgFuelConsumption);
+
+    refuellingfArray.forEach((item, index) => {
+      if (item === '-' && index > 0 && index < refuellingList.length - 1) {
+        refuellingfArray[index] = refuellingfArray[index - 1];
+      }
+    });
+
+    if (refuelling.fullRefuelling === false && index > 0) {
+      avgFuelConsumption = refuellingfArray[index - 1];
+    }
+
+    refuellingfArray2 = [
+      ...refuellingfArray2,
+      {
+        avg: refuellingfArray[index],
+        fullRefuelling: refuelling.fullRefuelling,
+        date: refuelling.date,
+        amount: refuelling.amount,
+        liters: refuelling.liters,
+        mileage: refuelling.mileage,
+        priceLiter: refuelling.priceLiter,
+      },
+    ];
+
+    // refuellingfArray2.push({
+    //   avg: refuellingfArray[index],
+    //   fullRefuelling: refuelling.fullRefuelling,
+    //   date: refuelling.date,
+    //   amount: refuelling.amount,
+    //   liters: refuelling.liters,
+    //   mileage: refuelling.mileage,
+    //   priceLiter: refuelling.priceLiter,
+    // });
+
+    console.log(refuellingfArray2);
+    return refuellingfArray2;
+  });
 }
 
 function* getData(action) {
@@ -16,13 +115,12 @@ function* getData(action) {
         action.payload.userEmail
       }/.json`,
     );
-    yield call(testt);
-    console.log(result);
-    // yield put(
-    //   actions.getDataSuccess({
-    //     data: result.data,
-    //   }),
-    // );
+    // yield call(calculateAvgConsumption(result.data));
+    yield put(
+      actions.getDataSuccess({
+        data: calculateAvgConsumption(result.data),
+      }),
+    );
   } catch (e) {
     console.log(e);
   }
